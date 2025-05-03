c6fefd964e1070b613e2c5d30a5a2f528f468308
N D Yogeshwar <yogeshwarnd69@gmail.com>
2025-05-03 11:18:21 +0530

    Create Binay search.c

---
+++ Binay search.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int binarysearch(int a[],int n,int key)
+{
+	int l=0,h=n-1,mid;
+	while(l<=h)
+	{
+		mid=(l+h)/2;
+		if(a[mid]==key)
+		return mid;
+		else if(a[mid]<key)
+		l=mid+1;
+		else
+		h=mid-1;
+	}
+	return -1;
+}
+int main()
+{
+	int a[]={1,2,3,4,5,6,7},n,key,result;
+	scanf("%d",&key);
+	n=sizeof(a)/sizeof(a[1]);
+	result=binarysearch(a,n,key);
+	if(result!=-1)
+	{
+	printf("yes");}
+	else
+	printf("no");
+	return 0;
+}
